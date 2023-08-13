a99f01385f8433ec994cc56d6462ae7dd376f9e3
David A. Viramontes <dviramontes@gmail.com>
2023-08-13 12:23:37 -0400

    wip: iframe setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,38 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// import default from @vimeo/player
+
+import { default as Player } from "@vimeo/player";
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
+    const options = {
+        id: 795682177,
+        width: 640,
+        loop: true
+    };
 
+    const player = new Player('playertwo', options);
+    //
+    // player.setVolume(0);
+    //
+    player.on('play', function() {
+        console.log('played the video!');
+    });
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>!!!</h1>
+
+      {/*<iframe*/}
+      {/*  src="https://player.vimeo.com/video/76979871?h=8272103f6e"*/}
+      {/*  width="640"*/}
+      {/*  height="360"*/}
+      {/*  frameBorder="0"*/}
+      {/*  allowFullScreen*/}
+      {/*  allow="autoplay; encrypted-media"*/}
+      {/*></iframe>*/}
     </>
   );
 }
