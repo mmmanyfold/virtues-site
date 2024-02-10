1a64fb476e8b36a65fba7c4885120235c44ba37a
David Viramontes <dviramontes@gmail.com>
2024-02-10 17:58:30 -0500

    render external links from notion db

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,23 @@
 import "react";
 import { useAtom } from "jotai";
+import { ArrowUpRight } from "@phosphor-icons/react";
 import {
   playlistsAtom,
   currentVideoIndexAtom,
   isAboutOpenAtom,
+  externalLinksPageAtom,
 } from "../store.ts";
 import { handleSetCurrentVideo, handleOpenAbout } from "../handlers.ts";
 import { PlaylistVideo } from "../types.ts";
 
-function MenuItem({ title, isCurrentView, onClick }: any) {
+function MenuItem({ title, isCurrentView, onClick, isSecondary }: any) {
   return (
     <div
       role="button"
-      className={`pb-0.5 border-b border-b-[3px] hover:border-b-[#000] ${
-        isCurrentView ? "border-b-[#000]" : "border-b-[#fcf3e9]"
+      className={`pb-0.5 border-b border-b-[3px] hover:border-b-[#000] hover:text-[#000] ${
+        isCurrentView
+          ? "border-b-[#000] text-[#000]"
+          : `border-b-[#fcf3e9] ${isSecondary ? "light-gray" : ""}`
       }`}
       onClick={onClick}
     >
@@ -22,22 +26,37 @@ function MenuItem({ title, isCurrentView, onClick }: any) {
   );
 }
 
+function ExternalLink({ link, text }: any) {
+  return (
+    <a
+      className="flex gap-x-1.5 items-center pb-0.5 border-b border-b-[3px] border-b-[#fcf3e9] hover:border-b-[#000] font-normal"
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {text}
+      <ArrowUpRight size={24} weight="bold" />
+    </a>
+  );
+}
+
 export default function Menu() {
   const [playlists] = useAtom(playlistsAtom);
   const [currentVideoIndex] = useAtom(currentVideoIndexAtom);
   const [isAboutOpen] = useAtom(isAboutOpenAtom);
   const orderedPlaylists = playlists.sort(
-    (a: PlaylistVideo, b: PlaylistVideo) => a.order - b.order
+    (a: PlaylistVideo, b: PlaylistVideo) => a.order - b.order,
   );
+  const [externalLinks] = useAtom(externalLinksPageAtom);
 
   return (
     <div className="absolute z-30 w-[100%] h-[100dvh] bg-[#fcf3e9] flex flex-col items-center justify-center text-2xl tracking-wide">
-      <div className="w-full h-100 overflow-scroll py-20">
+      <div className="w-full h-100 overflow-auto py-20">
         <div className="w-full flex flex-col items-center justify-center gap-y-6 text-center">
           {orderedPlaylists.map(
             (
               { uuid, videoTitle, vimeoPlayerURL }: PlaylistVideo,
-              index: number
+              index: number,
             ) => {
               const isCurrentVideo = index === currentVideoIndex;
               return (
@@ -48,10 +67,21 @@ export default function Menu() {
                   onClick={() => handleSetCurrentVideo(vimeoPlayerURL)}
                 />
               );
-            }
+            },
           )}
+          {externalLinks?.rows.length > 0 &&
+            externalLinks?.rows.map((link: any, index: number) => {
+              return (
+                <ExternalLink
+                  key={"elink-" + index}
+                  link={link.link}
+                  text={link.text}
+                />
+              );
+            })}
           <MenuItem
             title="About"
+            isSecondary={true}
             isCurrentView={isAboutOpen}
             onClick={handleOpenAbout}
           />
